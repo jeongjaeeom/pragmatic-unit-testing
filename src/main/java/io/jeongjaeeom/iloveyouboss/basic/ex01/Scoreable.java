16fcb3fce38e858ba47c3255442e69f4731bc24e
jeongjaeeom <jeongjae.eom@gmail.com>
2020-02-12 16:09:51 +0900

    First commit

---
+++ src/main/java/io/jeongjaeeom/iloveyouboss/basic/ex01/Scoreable.java
@@ -0,0 +1,8 @@
+package io.jeongjaeeom.iloveyouboss.basic.ex01;
+
+@FunctionalInterface
+public interface Scoreable {
+
+  int getScore();
+
+}
---
+++ src/test/java/io/jeongjaeeom/iloveyouboss/basic/ex01/ScoreCollectionTest.java
@@ -0,0 +1,26 @@
+package io.jeongjaeeom.iloveyouboss.basic.ex01;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ScoreCollectionTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void add() {
+  }
+
+  @Test
+  public void arithmeticMean() {
+  }
+}
